0981f3355931fdffe1acb9839680e291f810f5c8
KyungMin <2004ckck@naver.com>
2021-04-07 23:35:20 +0900

    [CREATE]  create 함수

---
+++ src/main/kotlin/Chap2/ObservableCreateExampleKotlin.kt
@@ -0,0 +1,22 @@
+package Chap2
+
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.ObservableEmitter
+
+class ObservableCreateExampleKotlin {
+
+    fun emit() {
+        val source = Observable.create { emitter: ObservableEmitter<Int> ->
+            emitter.onNext(100)
+            emitter.onNext(200)
+            emitter.onNext(300)
+
+        }
+        source.subscribe { data -> println("Result : $data") }
+    }
+}
+
+fun main() {
+    val demo=ObservableCreateExampleKotlin()
+    demo.emit()
+}
