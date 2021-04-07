e558290ddacf84c6fb68238754d6041034f8e265
KyungMin <2004ckck@naver.com>
2021-04-07 23:32:30 +0900

    [CREATE] HashSet 형식

---
+++ src/main/java/Chap2/ObservableFromIterableSetJava.java
@@ -0,0 +1,25 @@
+package Chap2;
+
+import io.reactivex.rxjava3.core.Observable;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ObservableFromIterableSetJava {
+    private void emit() {
+        Set<String> cities = new HashSet<>();
+        cities.add("Seoul");
+        cities.add("London");
+        cities.add("Paris");
+
+        Observable<String> source = Observable.fromIterable(cities);
+        source.subscribe(System.out::println);
+    }
+
+
+    public static void main(String[] args) {
+
+        ObservableFromIterableSetJava demo = new ObservableFromIterableSetJava();
+        demo.emit();
+    }
+}
---
+++ src/main/kotlin/Chap2/ObservableFromIterableSetKotlin.kt
@@ -0,0 +1,21 @@
+package Chap2
+
+import io.reactivex.rxjava3.core.Observable
+
+class ObservableFromIterableSetKotlin {
+
+    fun emit(){
+        val cities=HashSet<String>()
+        cities.add("Seoul")
+        cities.add("London")
+        cities.add("Paris")
+
+        val source= Observable.fromIterable(cities)
+        source.subscribe(System.out::println)
+    }
+}
+
+fun main(){
+    val demo = ObservableFromIterableSetKotlin()
+    demo.emit()
+}
