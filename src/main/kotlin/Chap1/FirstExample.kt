f7a53181e0384c2fc5b52e1a97d718c754197a08
KyungMin <2004ckck@naver.com>
2021-04-07 23:36:50 +0900

    [CREATE]   just() 함수

---
+++ src/main/kotlin/Chap1/FirstExample.kt
@@ -0,0 +1,15 @@
+package Chap1
+
+import io.reactivex.rxjava3.core.Observable
+
+class FirstExample {
+    fun emit(){
+        Observable.just("Hello","RxJava 2!!")
+            .subscribe(System.out::println)
+    }
+}
+
+fun main(){
+    val demo=FirstExample()
+    demo.emit()
+}
