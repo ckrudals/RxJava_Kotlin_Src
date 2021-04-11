869c03292ebee589c5873dc123bd985fd489459e
KyungMin <2004ckck@naver.com>
2021-04-11 18:55:08 +0900

    [CREATE] ObservableFromFuture function

---
+++ src/main/java/Chap2/ObservableFrmFuture.java
@@ -0,0 +1,26 @@
+package Chap2;
+
+import io.reactivex.rxjava3.core.Observable;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class ObservableFrmFuture {
+
+    private void emit() {
+        Future<String> future = Executors.newSingleThreadExecutor().submit(() -> {
+            Thread.sleep(1000);
+            return "Hello Future";
+        });
+        Observable<String> source = Observable.fromFuture(future);
+        source.subscribe(System.out::println);
+    }
+
+
+    public static void main(String[] args) {
+
+        ObservableFrmFuture demo = new ObservableFrmFuture();
+        demo.emit();
+        ;
+    }
+}
---
+++ src/main/kotlin/Chap2/ObservableFromFuture.kt
@@ -0,0 +1,20 @@
+package Chap2
+
+import io.reactivex.rxjava3.core.Observable
+import java.util.concurrent.Executors
+
+class ObservableFromFuture {
+    fun emit() {
+        val future = Executors.newSingleThreadExecutor().submit<String> {
+            Thread.sleep(1000)
+            "Hello Future"
+        }
+        val source= Observable.fromFuture(future)
+        source.subscribe(System.out::println)
+    }
+}
+
+fun main() {
+    val demo=ObservableFromFuture()
+    demo.emit()
+}
