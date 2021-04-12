6c8ae7b0a7b9544ee521be26b980451b050980e5
KyungMin <2004ckck@naver.com>
2021-04-12 23:30:53 +0900

    [CREATE] Observable 에서 Single 사용

---
+++ src/main/java/Chap2/SingleExampleObservable.java
@@ -0,0 +1,40 @@
+package Chap2;
+
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Single;
+
+public class SingleExampleObservable {
+
+    private void emit() {
+        Observable<String> source = Observable.just("Hello Single");
+        Single.fromObservable(source).subscribe(System.out::println);
+    }
+
+    private void single() {
+        Observable.just("Hello Single").single("Default item").subscribe(System.out::println);
+    }
+
+    private void first() {
+        String[] colors = {"Red", "Blue", "Gold"};
+        Observable.fromArray(colors).first("default value").subscribe(System.out::println);
+    }
+
+    private void empty() {
+        Observable.empty().single("default value").subscribe(System.out::println);
+
+    }
+
+    private void take() {
+        Observable.just(new Order("ORD-1"), new Order("ORD-2")).take(1).single(new Order("default order")).subscribe(System.out::println);
+    }
+
+
+    public static void main(String[] args) {
+        SingleExampleObservable demo=new SingleExampleObservable();
+        demo.emit();
+        demo.single();
+        demo.first();
+        demo.empty();
+        demo.take();
+    }
+}
---
+++ src/main/kotlin/Chap2/SingleExampleObservableKotlin.kt
@@ -0,0 +1,44 @@
+package Chap2
+
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Single
+
+class SingleExampleObservableKotlin {
+    fun emit() {
+        val source = Observable.just("Hello Single")
+        Single.fromObservable(source)
+            .subscribe { x -> println(x) }
+    }
+
+    fun single() {
+        Observable.just("Hello Single")
+            .single("default item")
+            .subscribe { x -> println(x) }
+    }
+
+    fun first() {
+        val colors = arrayOf("Red", "Blue", "Gold")
+        Observable.fromArray(*colors).first("default value").subscribe { x -> println(x) }
+    }
+
+    fun empty() {
+        Observable.empty<String>().single("default value")
+            .subscribe{x -> println(x) }
+    }
+
+    fun take(){
+        Observable.just(Order("ORD-1"),Order("ORD-2"))
+            .take(1)
+            .single(Order("default order"))
+            .subscribe { x -> println(x)}
+    }
+}
+
+fun main() {
+    val demo=SingleExampleObservableKotlin()
+    demo.emit()
+    demo.single()
+    demo.first()
+    demo.empty()
+    demo.take()
+}
